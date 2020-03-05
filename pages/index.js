3beb4d7ac9bf2b6306a95cc6067ec75a6d7d7ffe
Jerónimo Cosío <jerocosio@fb.com>
2020-03-05 01:20:43 -0600

    Este es el primer commit del proyecto.

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function IndexPage() {
+    return (
+        <div>
+            <p>
+                ¡Este es mi primer deploy con Next.js!
+            </p>
+        </div>
+    )
+}
+
+export default IndexPage
